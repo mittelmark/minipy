7b637c3eabd2cc744e0013c8bfc4f62c77f9d12c
Dr. rer. n Detlef Groth <detlef.groth@uni-potsdam.de>
2023-01-21 18:00:21 +0100

    extending add documentation

--- minipy/add.py
+++ minipy/add.py
@@ -1,4 +1,15 @@
 def add(x,y):
-    '''adds two numbers x and y and returns result'''
+    """Adds two numbers x and y and returns result.
+    
+    This is just an illustrative example function for a
+    minimal  Python package.
+    
+    Args:
+        x (int): an integer to be added
+        y (int): a second integer to be added
+        
+    Returns:
+        int : the value of x+y
+    """
     return(x+y)
     
